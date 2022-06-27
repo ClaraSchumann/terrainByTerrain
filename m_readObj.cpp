5347b4a203eaf8097493ac656ec140033ceaafdc
Scriabin <Scriabininoff@gmail.com>
2022-06-27 16:39:31 +0800

    Rewrite multi-thread part, avoid using std::vector

--- m_readObj.cpp
+++ m_readObj.cpp
@@ -16,11 +16,34 @@ Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> std_vector_to_Eigen(const std::
 	return ret;
 };
 
+template<typename T>
+Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> pointers_to_Eigen(const std::vector<std::pair<T*, size_t>>& entries) {
+	size_t rows = 0;
+	for (auto& e : entries) {
+		rows += e.second / 3;
+	}
+	std::cout << fmt::format("Gathering {} rows.\n", rows);
+	Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> ret(rows, 3);
+	size_t i = 0;
+	for (auto& e : entries) {
+		T* buffer = e.first;
+		size_t r = e.second;
+		for (size_t j = 0; j < r;) {
+			ret(i, 0) = buffer[j++];
+			ret(i, 1) = buffer[j++];
+			ret(i, 2) = buffer[j++];
+			i++;
+		}
+	}
+	assert(ret(rows - 1, 2) != 0);
+	return ret;
+}
+
 // In case the forth parameter of the returned Eigen matrix in function 'std_vector_to_Eigen' be modified. 
 template<typename T>
 struct cvtfunc_get_type {
-	using eigen_type = std::invoke_result<decltype(std_vector_to_Eigen<T>), std::vector<std::vector<T>>>::type;
-	using vector_type = std::vector<eigen_type>;
+	using eigen_type = std::invoke_result<decltype(pointers_to_Eigen<T>), const std::vector<std::pair<T*, size_t>>&>::type;
+	using vector_type = std::vector<std::pair<T*, size_t>>;
 };
 
 std::tuple<Eigen::MatrixXd, Eigen::MatrixXi> m_readObj(const std::string& path) {
@@ -31,8 +54,6 @@ std::tuple<Eigen::MatrixXd, Eigen::MatrixXi> m_readObj(const std::string& path) 
 	p = std::filesystem::absolute(p);
 	std::cout << fmt::format("Reading .obj file {} \n", p.string());
 
-	Eigen::MatrixXd V;
-	Eigen::MatrixXi F;
 	std::ifstream infile(p.string(), std::ios::in);
 	infile.seekg(0, std::ios_base::end);
 	size_t length = infile.tellg();
@@ -53,8 +74,8 @@ std::tuple<Eigen::MatrixXd, Eigen::MatrixXi> m_readObj(const std::string& path) 
 	// For debugging.
 	//num_threads = 1;
 
-	cvtfunc_get_type<int>::vector_type faces_gather(num_threads);
-	cvtfunc_get_type<double>::vector_type vertices_gather(num_threads);
+	cvtfunc_get_type<double>::vector_type vertices_raw(num_threads);
+	cvtfunc_get_type<int>::vector_type faces_raw(num_threads);
 
 	// Under the assumption that each line ends up with a '\n', only 'v' and 'f' elements are parsed.
 	std::vector<size_t> line_headers;
@@ -73,12 +94,13 @@ std::tuple<Eigen::MatrixXd, Eigen::MatrixXi> m_readObj(const std::string& path) 
 	delimiters.push_back(line_headers.size());
 
 	auto worker = [&](size_t begin, size_t end, size_t thread_index) {
-		thread_local std::vector local_line_headers(line_headers.begin() + begin, line_headers.begin() + end);
-		thread_local std::vector<std::vector<double>> vertices_s;
-		thread_local std::vector<std::vector<int>> faces_s;
-		std::cout << fmt::format("Thread{}, from {} to {}, {} in total", thread_index, begin, end, end - begin);
+		std::vector local_line_headers(line_headers.begin() + begin, line_headers.begin() + end);
+		std::cout << fmt::format("Thread {}, from {} to {}, {} in total", thread_index, begin, end, end - begin);
 		end -= begin;
 		begin -= begin;
+		double* v_buffer = new double[end * 3];
+		int* f_buffer = new int[end * 3];
+		size_t v_loc = 0, f_loc = 0;
 		for (auto i = begin; i < end; i++) {
 			if ((i - begin) % 100000 == 0) {
 				std::cout << fmt::format("Thread {} : {} / {}, {}%\n", thread_index, i, end, float(i) / end * 100) << std::flush;
@@ -89,30 +111,28 @@ std::tuple<Eigen::MatrixXd, Eigen::MatrixXi> m_readObj(const std::string& path) 
 				if (buffer[local_line_headers[i] + 1] != ' ' && buffer[local_line_headers[i] + 1] != '\t')
 					continue;
 				char* tmp = buffer + local_line_headers[i] + 2;
-				double v1, v2, v3;
-				v1 = std::strtod(tmp, &tmp);
-				v2 = std::strtod(tmp, &tmp);
-				v3 = std::strtod(tmp, &tmp);
+				v_buffer[v_loc++] = std::strtod(tmp, &tmp);
+				v_buffer[v_loc++] = std::strtod(tmp, &tmp);
+				v_buffer[v_loc++] = std::strtod(tmp, &tmp);
 				//sscanf_s(buffer + line_headers[i], "v %lf %lf %lf\n", &v1, &v2, &v3);
-				vertices_s.push_back({ v1,v2,v3 });
+				//vertices_s.push_back({ v1,v2,v3 });
 				break;
 			}
 			case 'f': {
 				if (buffer[local_line_headers[i] + 1] != ' ' && buffer[local_line_headers[i] + 1] != '\t')
 					continue;
 				char* tmp = buffer + local_line_headers[i] + 2;
-				int v1, v2, v3;
-				v1 = std::strtoll(tmp, &tmp, 10);
+				f_buffer[f_loc++] = std::strtoll(tmp, &tmp, 10);
 				while (*tmp != ' ' && *tmp != '\t') {
 					tmp++;
 				};
-				v2 = std::strtoll(tmp, &tmp, 10);
+				f_buffer[f_loc++] = std::strtoll(tmp, &tmp, 10);
 				while (*tmp != ' ' && *tmp != '\t') {
 					tmp++;
 				};
-				v3 = std::strtoll(tmp, &tmp, 10);
+				f_buffer[f_loc++] = std::strtoll(tmp, &tmp, 10);
 				//sscanf_s(buffer + line_headers[i], "f %llu %llu %llu\n", &v1, &v2, &v3);
-				faces_s.push_back({ v1,v2,v3 });
+				//faces_s.push_back({ v1,v2,v3 });
 				break;
 			}
 			default: {
@@ -124,31 +144,30 @@ std::tuple<Eigen::MatrixXd, Eigen::MatrixXi> m_readObj(const std::string& path) 
 			}
 		}
 
-		try {
-			std::unique_lock<std::mutex> u_lock(mtx);
-			// Some parts may contain no faces or vertices.
-			if (faces_s.size()) {
-				faces_gather[thread_index] = std::move(std_vector_to_Eigen(faces_s));
-			}
-			if (vertices_s.size()) {
-				vertices_gather[thread_index] = std::move(std_vector_to_Eigen(vertices_s));
-			}
-		}
-		catch(std::exception e){
-			std::cout << e.what();
-		}
+		assert(v_loc % 3 == 0);
+		assert(f_loc % 3 == 0);
+		std::unique_lock<std::mutex> u_lock(mtx);
+		vertices_raw[thread_index] = std::make_pair(v_buffer, v_loc);
+		faces_raw[thread_index] = std::make_pair(f_buffer, f_loc);
 	};
 
 	std::vector<std::thread> threads;
 	for (size_t i = 0; i < num_threads; i++) {
-		threads.push_back(std::thread(worker, delimiters[i], delimiters[i + 1],i));
+		threads.push_back(std::thread(worker, delimiters[i], delimiters[i + 1], i));
 	}
 
 	for (auto& t : threads) {
 		t.join();
 	}
 
+	auto V = pointers_to_Eigen(vertices_raw);
+	auto F = pointers_to_Eigen(faces_raw);
 
-
-	return {};
+	for (auto& p : vertices_raw) {
+		delete[] p.first;
+	}
+	for (auto& p : faces_raw) {
+		delete[] p.first;
+	}
+	return {V,F};
 }
